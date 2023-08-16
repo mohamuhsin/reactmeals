e55ab4c781e4f8a838ee2b0fb4c1bf247ae98dc9
mohamuhsin <muhsinohoho@outlook.com>
2023-08-16 16:07:06 +0300

    started working on heaer component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header>
+        <h1>React Meals</h1>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
