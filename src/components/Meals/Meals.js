8aff40ca69f711617a495f7b04d6e548e4c7fbdd
mohamuhsin <muhsinohoho@outlook.com>
2023-08-17 21:32:58 +0300

    started working on meals

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import 
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+export default Meals;
