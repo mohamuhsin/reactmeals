21a58efcc5e679e00d6800f2ecb710afad401012
mohamuhsin <muhsinohoho@outlook.com>
2023-08-17 21:16:15 +0300

    styled the Cart Icon

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,14 @@
 import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartIcon.module.css";
 
 const HeaderCartButton = () => {
   return (
-    <button>
-      <span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span>3</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
