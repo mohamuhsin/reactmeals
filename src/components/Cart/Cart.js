171f59bb1f31b43e53d5a2395da01161819155a4
mohamuhsin <muhsinohoho@outlook.com>
2023-08-19 15:52:16 +0300

    started working on the logic of the cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import classes from "./Cart.module.css";
+const Cart = (props) => {
+  return (
+    <div>
+      cartItems
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Cart;
