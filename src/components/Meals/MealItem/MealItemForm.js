e1e2df4a3dcd538dbd646df1be5d5a5f5550e863
mohamuhsin <muhsinohoho@outlook.com>
2023-08-19 20:02:12 +0300

    using refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,18 @@
+import { useRef } from "react";
+
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="amount"
         input={{
           id: "amount_" + props.id,
